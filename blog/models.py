118dbfb2dc722a401fbb1b1a1bd9b6ea1ab6438e
Kassem79 <sherifkassem@hotmail.com>
2026-02-26 11:55:19 +0000

    Add posts text

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,4 @@
-from tokenize import Comment
-
+# from tokenize import Comment
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -10,8 +9,8 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="blog_posts"
-)
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
@@ -19,8 +18,8 @@ class Post(models.Model):
     updated_on = models.DateTimeField(auto_now=True)
 
 
-class Meta:
-    ordering = ["-created_on"]
+    class Meta:
+        ordering = ["-created_on"]
     
     def __str__(self):
         return f"{self.title} | written by {self.author}"
