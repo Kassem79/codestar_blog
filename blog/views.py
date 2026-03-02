61c301806db5369e49cdde6c0ab2d66d7edb5768
Kassem79 <sherifkassem@hotmail.com>
2026-03-02 12:20:12 +0000

    Add form file contains colloabrate form

--- blog/views.py
+++ blog/views.py
@@ -33,6 +33,8 @@ def post_detail(request, slug):
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
     if request.method == "POST":
+        print("Received a Post request")
+    
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
@@ -45,7 +47,7 @@ def post_detail(request, slug):
             )
 
     comment_form = CommentForm()
-
+    print("About to render template")
     return render(
         request, 
         "blog/post_detail.html", 
